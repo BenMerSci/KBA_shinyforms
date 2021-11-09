cc4b6b7a5a4c24e7fd3d8d9435154531701b09db
BenMerSci <benjamin.b.mercier@usherbrooke.ca>
2021-11-09 00:14:12 -0500

    Added final ui elements

--- app.R
+++ app.R
@@ -1,3 +1,4 @@
+# Needed libraries to run the app
 library(shiny)
 library(bslib)
 library(tidyverse)
@@ -15,82 +16,85 @@ library(emo)
 # Informations to authenticate are stored in an .Rprofile
 googledrive::drive_auth()
 
-# Create KBA summaries
 # User interface
 ui <- fluidPage(
 
-    tags$head(
+# Calling of shinyjs package for functionality in `server`
+shinyjs::useShinyjs(),
+theme = bs_theme(bg = "#f6f6f6", fg = "#1D4044", base_font = font_google("Lato"), "progress-bar-bg" = "orange"),
+
+# Change web window title + adding the CSS styling 
+  tags$head(
+    HTML("<title> Create KBA summary </title>"),
+    tags$div(HTML("<a id='KBA_logo' href='https://kbacanadawiki.org/'><img src='./Canada_KBA_transparent_white.png' alt='KBA_logo' style='position: fixed; top: 4%; left: 6%; width: 5%; height: 5%;'></a>")),
     includeCSS("www/styles.css")
   ),
 
-shinyjs::useShinyjs(),
-theme = bs_theme(bg = "#FFFAF0", fg = "#1D4044", base_font = font_google("Lato"), "progress-bar-bg" = "orange"),
+# Add the titlepanel which is modified in the .css file
+titlePanel(h1("Creation of KBA summaries", class = "h1")),
 
- HTML("<h1>Creation of KBA summaries</h1>"),
+# Spaces before the panels
+    br(),
 
-    br(),
-    br(),
-    
-  sidebarLayout(
-    
-    sidebarPanel(id = "sidebar", width = 3,  
-      h3("ReadMe", align = "center"),
-      "This Shiny application is intended to convert KBA Canada proposal forms into KBA summaries for expert review.",
+fluidRow( 
+	column(width = 3, offset = 1,
+	wellPanel(class = "well", h2("ReadMe"),
       hr(),
-      h4("How to proceed:"),
-      h5("1- Select review stage."),
-      h5("2- Upload the desired proposals to convert."),
-      h5("3- Click convert button to summarize."),
-      h5("4- Check result table to see which proposals were correctly processed."),
-      h5("5- Download!"),
+      h4("This Shiny application is intended to convert KBA Canada proposal forms into KBA summaries for expert review."),
+      h3("How to proceed:"),
       hr(),
-      h6("Developed by Benjamin Mercier and Chloé Debyser for the KBA Canada Secretariat"),
+      h4("1- Select review stage."),
+      h4("2- Upload the desired proposals to convert."),
+      h4("3- Click convert button to summarize."),
+      h4("(Process might take a couple seconds to start)"),
+      h4("4- Check result table to see which proposals were correctly processed."),
+      h4("5- Download!"),
+      hr(),
+      h5("Developed by Benjamin Mercier and Chloé Debyser for the KBA Canada Secretariat"),
+      br(),
       h6("Source code", tags$a(href="https://github.com/BenMerSci/KBA_shinyforms", icon("github","fa-2x"))),
-      tags$style(".fa-github {color:#13294B}")
-    ),
+      tags$style(".fa-github {color:#13294B}"),
+)),
 
-    mainPanel(
-
-    fluidRow(
-      column(width = 7, offset = 1,
-        wellPanel(
-          fluidRow(
-            column(width = 8,
-              radioButtons("stageRev", h4("Select review stage"),
+        column(width = 4,
+	wellPanel(class = "well", h2("Input"),
+          hr(),
+              radioButtons("stageRev", h3("Select review stage"),
                           choices = list("Technical review" = "technicalRev",
                                    "General review" = "generalRev",
                                    "Steering Committee" = "steeringRev"),
-                          selected = "technicalRev", inline = TRUE)
-            )
-          ),
-      
-          fluidRow(
-            column(width = 8,
-              fileInput("file", label = h4("Upload your proposal(s)"),
+                          selected = "technicalRev", inline = TRUE),
+        
+              fileInput("file", label = h3("Upload your proposal(s)"),
                          placeholder = "or drop files here",
                          multiple = TRUE,
-                         accept = c(".xlsx", ".xlsm", ".xls"),
-                         width = '100%')
-            )
-          )
-        ) 
-      )
-    ),
+                         accept = c(".xlsm"),
+                         width = '120%'),
+        hr(),
+        br(),
+        br(),
+        br(),
+        br(),
+	      tags$div(uiOutput('runButton'), align = "center"),
+        )),
 
 
+      column(width = 4,
+          wellPanel(class = "well_scroll", h2("Output"),
+	  hr(),
+    br(),
+      tags$div(downloadButton("downloadData", "Download"), align = "center"),
+    br(),
+      tags$div(tableOutput("resTable"), align = "center"),
+        )),
 
+  
+  ),
 
-      fluidRow(
-        column(width = 2, offset = 1, uiOutput('runButton')),
-        column(width = 2, offset = 1, downloadButton("downloadData", "Download"))
-      ),
-
-      br(),
-      br(),
-
-      fluidRow(
-        column(width = 8, offset = 1, tableOutput("resTable")),
-      )
+fluidRow(
+    column(12,
+    tags$div(class = "footer", HTML("Photograph © Tony Webster, <a rel='noreferrer noopener' href='https://www.flickr.com/photos/diversey/22492133101/in/photolist-Agy4yi-4hP5D-2jiQL9m-2iqMZ89-Q4p8C3-gAnYRA-24Z34E4-PuPSu3-2kjavbz-2kbsmz1-2iqP7Fu-nmFr4C-zbb2AM-qeoBfK-nYdH9B-RASxd7-Qd1Sgq-2f2oPXZ-qzAw71-8MVKVd-T7niUj-etMN7D-DkStuM-55gdqb-2j8xytN-qcnKee-gAo3X9-ur89y7-gAkQjM-5iBJJR-diC8TQ-aS2LbM-pq2Y4k-5iG1qG-5iBJiZ-g88dhq-2j8xywt-5iBJh8-u9XuRf-PE5GWE-2kJ5z3N-5iG1SY-JTZ92V-5iBJBn-3gaoc2-Cudqzt-2koJsqF-2jd9VDJ-6y7wxz-dk4Aui/' target='_blank'>Lake of Shining Waters - Prince Edward Island National Park</a> 
+    (cropped) <a rel='noreferrer noopener' href='https://creativecommons.org/licenses/by-sa/2.0/' target='_blank'>CC BY-SA 2.0</a>"))
     )
   )
 )
@@ -152,4 +156,8 @@ shinyjs::hide('downloadData')
   
 }
 
+
 shinyApp(ui, server)
+
+
+
